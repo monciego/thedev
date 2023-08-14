9fcaaac5b2901dc3b09e5c75bcc9b92aa90c616c
Jericho <jerichobantiquete@gmail.com>
2023-08-14 22:33:37 +0800

    build: add fonts

---
+++ app/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Syncopate } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const syncopate = Syncopate({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+});
