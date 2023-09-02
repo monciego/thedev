e5c6068c607cb47ed42bf210f6c472b4389b9ccd
Jericho <jerichobantiquete@gmail.com>
2023-09-02 21:37:46 +0800

    feat: add cta section

--- app/page.tsx
+++ app/page.tsx
@@ -144,6 +144,53 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className='mx-auto max-w-7xl sm:px-6 lg:px-8 my-4 mb-10'>
+        <div className='relative isolate overflow-hidden bg-slate-950  px-6 py-16 shadow-3xl sm:rounded-2xl'>
+          <svg
+            viewBox='0 0 1024 1024'
+            className='absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0'
+            aria-hidden='true'
+          >
+            <circle
+              cx='512'
+              cy='512'
+              r='512'
+              fill='url(#759c1415-0410-454c-8f7c-9a820de03641)'
+              fillOpacity='0.7'
+            />
+            <defs>
+              <radialGradient id='759c1415-0410-454c-8f7c-9a820de03641'>
+                <stop stopColor='#c775d6' />
+                <stop offset='1' stopColor='#2328c2' />
+              </radialGradient>
+            </defs>
+          </svg>
+          <div className='mx-auto max-w-2xl text-center '>
+            <h2 className='text-2xl font-bold tracking-tight text-white sm:text-4xl'>
+              Let’s Connect and Build Together
+            </h2>
+            <p className='mt-6 text-base sm:text-lg leading-8 text-gray-300'>
+              We’re excited to hear about your project and discuss how we can
+              help you achieve your goals. Our team is here to assist you every
+              step of the way.
+            </p>
+            <div className='mt-10 flex items-center justify-center gap-x-6 '>
+              <a
+                href='#'
+                className='rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
+              >
+                Contact Us
+              </a>
+              <a
+                href='#'
+                className='text-sm font-semibold leading-6 text-white'
+              >
+                Learn more <span aria-hidden='true'>→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
