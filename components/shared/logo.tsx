3f5a4f4aff857f3573f3cdf89072891dad9788a1
monciego <jerichobantiquete@gmail.com>
2024-02-25 19:17:25 +0800

    feat: change logo

--- components/shared/logo.tsx
+++ components/shared/logo.tsx
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../public/logo.png';
 
 type LogoProps = {};
 
 const Logo = ({}: LogoProps): JSX.Element => {
-  return <Link href={`/`}>Logo</Link>;
+  return (
+    <Link href={`/`}>
+      <Image className='w-12' src={logo} alt='thedev logo' />
+    </Link>
+  );
 };
 
 export default Logo;
